a76deddc8cfeed23df0477a6eadb666c18247522
Enryuu13 <denniseliasmaradiaga@gmail.com>
2024-11-17 22:36:28 -0600

    Actualización

--- sistema-gestion-horas/src/main/java/grupo3programacionavanzada/data/EmpleadosResponse.java
+++ sistema-gestion-horas/src/main/java/grupo3programacionavanzada/data/EmpleadosResponse.java
@@ -1,5 +1,18 @@
 package grupo3programacionavanzada.data;
 
 public class EmpleadosResponse {
-
+	private List<Empleado> items ;
+	private int count;
+	public List<Empleado> getItems() {
+		return items;
+	}
+	public void setItems(List<Empleado> items) {
+		this.items = items;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
 }
